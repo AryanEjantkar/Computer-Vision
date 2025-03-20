33a13c272c04cc7f88677aed34c64bd4af9117d4
AryanEjantkar <145706433+AryanEjantkar@users.noreply.github.com>
2025-03-20 13:17:55 +0530

    Add files via upload

---
+++ disp img using cv .py
@@ -0,0 +1,10 @@
+import random
+import cv2
+import matplotlib.pyplot as plt 
+import PIL as pillow
+import numpy as np
+
+img =cv2.imread('dog.4532.jpg')
+name=' Open cv pyton image'
+plt.title(name)
+plt.imshow(img)
